53ea5a48baa3ac51f51ea5eebfeca0b5f92c87f8
en14c <34634666+En14c@users.noreply.github.com>
2018-08-30 01:20:08 +0200

    add blueprint for App's authentication system and initialize the LoginManager instance from flask-login

--- app/__init__.py
+++ app/__init__.py
@@ -1,6 +1,7 @@
 from flask import Flask
 from flask_sqlalchemy import SQLAlchemy
 from flask_migrate import Migrate
+from flask_login import LoginManager
 from .main import main as main_blueprint
 
 
@@ -8,6 +9,9 @@ from .main import main as main_blueprint
 app_database = SQLAlchemy()
 app_migrate = Migrate(db=app_database)
 
+app_login_manager = LoginManager()
+app_login_manager.session_protection = 'strong'
+app_login_manager.login_view = 'auth.signin'
 
 def create_app(config):
     app = Flask(__name__)
@@ -15,7 +19,12 @@ def create_app(config):
 
     app_database.init_app(app)
     app_migrate.init_app(app)
+    app_login_manager.init_app(app)
+
+    from .main import main as main_blueprint
+    from .auth import auth as auth_blueprint
 
     app.register_blueprint(main_blueprint)
+    app.register_blueprint(auth_blueprint, url_prefix='/auth')
 
     return app
