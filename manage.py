9ccfa59927e02c58a5ac5fa92b541abaa293c877
en14c <34634666+En14c@users.noreply.github.com>
2018-09-25 00:32:36 +0200

    add a custom command to automate the generation of fake users/answers/questions/followers for the development env

--- manage.py
+++ manage.py
@@ -1,5 +1,4 @@
-import os
-import unittest
+import os, unittest
 from app import create_app, app_database
 from app.models import User, Role, Question, Answer, AppPermissions, Follow
 from confg import app_config
@@ -13,6 +12,38 @@ def app_test():
     test_suite = unittest.TestLoader().discover('tests')
     unittest.TextTestRunner(verbosity=2).run(test_suite)
 
+@app.cli.command()
+def generate_random_data():
+    """ generates random data for the development enivronment """
+    app_database.create_all()
+
+    Role.populate_table()
+    User.generate_fake_users()
+
+    print('[+] generating random data...')
+    print('[+] use the following user accounts to login and test the application\n')
+
+    for i in range(0, 3):
+        user = User.query.get(i + 1)
+        User.generate_fake_followers(user.username)
+        User.generate_fake_followed_users(user.username)
+        User.generate_fake_questions(user.username)
+        User.generate_fake_answers(user.username)
+
+        
+        #make sure that at least 3 followed users have fake Q&A to fill the the user's home page
+        followed_users = user.get_followed_users_list()[0:3]
+        for followed_user in followed_users:
+            User.generate_fake_followers(followed_user.username)
+            User.generate_fake_followed_users(followed_user.username)
+            User.generate_fake_questions(followed_user.username)
+            User.generate_fake_answers(followed_user.username)
+
+        user.password = '123'
+        print('username:{username}\nemail:{email}\npassword: {password}\n\n'.format(username=user.username,
+                                                                                    email=user.email,
+                                                                                    password='123'))
+    print('[+] done')
 
 @app.shell_context_processor
 def create_shell_context():
